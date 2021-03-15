161bb334f64a4a6d9218499aa70e43c9481992db
gediminasjuska <g.juska98@gmail.com>
2021-03-15 20:51:26 +0200

    update

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-let countDownDate = new Date("Mar 15, 2021 22:00:00").getTime();
+let countDownDate = new Date();
 
 let x = setInterval(function () {
   let now = new Date().getTime();
-  let left = countDownDate - now;
+  let left = countDownDate.setHours(22, 00, 00) - now;
 
   let hours = Math.floor((left % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   let minutes = Math.floor((left % (1000 * 60 * 60)) / (1000 * 60));
